f4cf8e67f2e10e910d371f2f494715d42f291078
Jeroen Resoort <jeroen@resoort.net>
2025-08-20 21:03:11 +0200

    reverse (#3)
    * keyboard input using unicurses
    
    * handle keyboard input and timing events synchronously in main loop
    
    * fix merge

--- src/step2controlled.py
+++ src/step2controlled.py
@@ -13,6 +13,10 @@ def main():
         uc.wmove(stdscr, 0, 0)
         uc.waddstr(stdscr, "Press keys (ESC to exit):")
         uc.wrefresh(stdscr)
+        uc.timeout(0)
+
+        last_beep = 0
+        reverse = False
 
         while True:
             ch = uc.wgetch(stdscr)
@@ -21,18 +25,29 @@ def main():
 
             if ch == 258 or ch == 456 or ch == 115:  # DOWN key mac, win, s
                 bot.doMove(-100, -100)
+                reverse = True
 
             if ch == 259 or ch == 450 or ch == 119:  # UP key mac, win, w
                 bot.doMove(200, 200)
+                reverse = False
 
             if ch == 260 or ch == 452 or ch == 97:  # LEFT key mac, win, a
                 bot.doMove(-100, 100)
+                reverse = False
 
             if ch == 261 or ch == 454 or ch == 100:  # RIGHT key mac, win, d
                 bot.doMove(100, -100)
+                reverse = False
 
             if ch == 32:   # SPACE key
                 bot.doMove(0, 0)
+                reverse = False
+
+            if (reverse):
+                now = time.time()
+                if now - last_beep > 1.2:  # every 1.2 seconds
+                    bot.doBuzzer(1200, 300)  # 300 milliseconds beep
+                    last_beep = now
 
             uc.wclear(stdscr)
             uc.wmove(stdscr, 0, 0)
@@ -45,8 +60,10 @@ def main():
                 char_repr = 'non-printable'
 
             uc.wmove(stdscr, 1, 0)
-            uc.waddstr(stdscr, f"Key pressed: {ch} ({char_repr})")
+            uc.waddstr(stdscr, f"Key pressed: {ch} ({char_repr})")  # -1 means "no key pressed"
             uc.wrefresh(stdscr)
+
+            sleep(0.1)
 
     finally:
         uc.keypad(stdscr, False)
