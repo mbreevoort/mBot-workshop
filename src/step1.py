5dd6b8d4ecadc5c2b8c72d15163a0c5d7f556338
michel.breevoort <3529964+mbreevoort@users.noreply.github.com>
2025-08-23 12:41:45 +0200

    Both led lights

--- src/step1.py
+++ src/step1.py
@@ -7,13 +7,14 @@ if __name__ == '__main__':
     try:
         while True:
             bot.doRGBLedOnBoard(1, 255, random.randint(0, 255), random.randint(0, 255))
-            sleep(1)
-            bot.doRGBLedOnBoard(1, random.randint(0, 255), 255, 0)
-            sleep(1)
+            sleep(0.5)
+            bot.doRGBLedOnBoard(2, random.randint(0, 255), 255, 0)
+            sleep(0.5)
             bot.doRGBLedOnBoard(1, random.randint(0, 255), 0, 255)
-            sleep(1)
-            bot.doRGBLedOnBoard(1, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            sleep(1)
+            sleep(0.5)
+            bot.doRGBLedOnBoard(2, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            sleep(0.5)
     finally:
-        bot.doRGBLedOnBoard(1, 0, 0, 0) # turn off the led lights
+        bot.doRGBLedOnBoard(1, 0, 0, 0) # Right turn off the led lights
+        bot.doRGBLedOnBoard(2, 0, 0, 0)  #Left turn off the led lights
 
