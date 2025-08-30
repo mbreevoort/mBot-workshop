0f44d944a01d4a0f502abb2b2141f221e9a4cb7e
michel.breevoort <3529964+mbreevoort@users.noreply.github.com>
2025-08-30 19:40:07 +0200

    add assigments and install tools pdf

--- install_libs.bat
+++ install_libs.bat
@@ -1,4 +1,5 @@
-%USERPROFILE%\AppData\Local\Programs\Python\Python312\Scripts\pip.exe install cython
-%USERPROFILE%\AppData\Local\Programs\Python\Python312\Scripts\pip.exe install pyserial
-%USERPROFILE%\AppData\Local\Programs\Python\Python312\Scripts\pip.exe install hidapi
-%USERPROFILE%\AppData\Local\Programs\Python\Python312\Scripts\pip.exe install pygame
+%USERPROFILE%\AppData\Local\Programs\Python\Python313\Scripts\pip.exe install cython
+%USERPROFILE%\AppData\Local\Programs\Python\Python313\Scripts\pip.exe install pyserial
+%USERPROFILE%\AppData\Local\Programs\Python\Python313\Scripts\pip.exe install hidapi
+%USERPROFILE%\AppData\Local\Programs\Python\Python313\Scripts\pip.exe install pygame
+%USERPROFILE%\AppData\Local\Programs\Python\Python313\Scripts\pip.exe install uni-curses
